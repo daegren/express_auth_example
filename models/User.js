3ef890b647ea2ee0f29e2bd0f7596b915deb4984
David Mills <dave@1morepx.com>
2018-03-09 10:54:30 -0500

    Add password to users
    Code from class March 9th, 2018

--- models/User.js
+++ models/User.js
@@ -1,49 +1,75 @@
-const uuid = require('uuid/v4');
+const uuid = require("uuid/v4");
+const { hash, verify } = require("../util/password");
 
 module.exports = knex => {
-  const registerUser = username =>
-    knex('users')
-      .insert({ username })
-      .returning('*');
+  const registerUser = (username, password) =>
+    hash(password).then(password_hashed =>
+      knex("users")
+        .insert({ username, password_hashed })
+        .returning("*")
+    );
 
-  const loginUser = username => findByUsername(username);
+  const loginUser = (username, password) =>
+    new Promise((resolve, reject) => {
+      let foundUser;
+
+      findByUsername(username)
+        .then(([user]) => {
+          if (user) {
+            foundUser = user;
+            return verify(password, user.password_hashed);
+          } else {
+            reject("user not found");
+          }
+        })
+        .then(valid => {
+          if (valid) {
+            resolve(foundUser);
+          } else {
+            reject("Invalid Password");
+          }
+        })
+        .catch(e => {
+          reject("user not found");
+        });
+    });
 
   const generateLoginToken = user =>
-    knex('users')
+    knex("users")
       .where({ id: user.id })
       .update({ token: uuid() })
-      .returning('*');
+      .returning("*");
 
   const find = id =>
-    knex('users')
+    knex("users")
       .where({ id })
-      .select('*')
+      .select("*")
       .limit(1);
 
   const findByUsername = username =>
-    knex('users')
+    knex("users")
       .where({ username })
-      .select('*')
+      .select("*")
       .limit(1);
 
   const findByToken = token =>
-    knex('users')
+    knex("users")
       .where({ token })
-      .select('*')
+      .select("*")
       .limit(1);
 
   return {
-    register: username =>
+    register: (username, password) =>
       new Promise((resolve, reject) => {
-        registerUser(username)
+        registerUser(username, password)
           .then(([user]) => generateLoginToken(user))
           .then(([user]) => resolve(user))
           .catch(e => reject(e));
       }),
-    login: username =>
+    login: (username, password) =>
       new Promise((resolve, reject) => {
-        loginUser(username)
-          .then(([user]) => generateLoginToken(user))
+        loginUser(username, password)
+          .then(user => generateLoginToken(user))
           .then(([user]) => resolve(user))
           .catch(e => reject(e));
       }),
